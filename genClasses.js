548e6019f8ffe55710a06418500f2b63bae61bfa
William Lee-Anglin <wla24@bath.ac.uk>
2023-09-18 22:31:19 +0100

    Fix flushes, two pairs, correct combos

--- genClasses.js
+++ genClasses.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const ranks = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6'];
-const rankNames = ['Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine', 'Eight', 'Seven', 'Six'];
+const rankNames = ['Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine', 'Eight', 'Seven', 'Sixe'];
 
 const handRanks = ['SF', '5K', '5F', '4K', 'FH', '4F', 'S', '3K', '2P', '1P', 'HC'];
 
@@ -9,41 +9,64 @@ const straights = [];
 
 const stream = fs.createWriteStream('wideDeckEquivalence.txt');
 
+let totalHands = 0;
+
+stream.write('CLASS\t\t# OF HANDS\t\tCLASS NAME\n\n')
+
 straightFlush(stream);
+console.log("Straight flush cumulative hands: " + totalHands);
 fiveKind(stream);
+console.log("Five of a kind cumulative hands: " + totalHands);
 fiveFlush(stream);
+console.log("Five flush cumulative hands: " + totalHands);
 fourKind(stream);
+console.log("Four of a kind cumulative hands: " + totalHands);
 fullHouse(stream);
+console.log("Full house cumulative hands: " + totalHands);
 fourFlush(stream);
+console.log("Four flush cumulative hands: " + totalHands);
 straight(stream);
+console.log("Straight cumulative hands: " + totalHands);
 threeKind(stream);
+console.log("Three of a kind cumulative hands: " + totalHands);
 twoPair(stream);
+console.log("Two pair cumulative hands: " + totalHands);
 onePair(stream);
+console.log("One pair cumulative hands: " + totalHands);
 highCard(stream);
+console.log("High card cumulative hands: " + totalHands);
+
+console.log("Total hands = " + totalHands);
+console.log("Expected total hands = 3162510");
 
 function straightFlush(stream) {
     for(i = 0; i < ranks.length - 3; i++) { 
         const str = ranks[i] + ranks[i + 1] + ranks[i + 2] + ranks[i + 3] + ranks[(i + 4)%ranks.length];
-        stream.write(str + '\t' + 6 + '\t' + rankNames[i] + ' high straight flush' + '\n');
+        straights.push(str); 
+        stream.write(str + '\t\t' + 6 + '\t\t' + rankNames[i] + ' high straight flush' + '\n');
+        totalHands+=6;
     }
 }
 
 function fiveKind(stream) {
     for(let i = 0; i < ranks.length; i++) {
         const fiveK = ranks[i].repeat(5);
-        stream.write(fiveK + '\t' + 6 + '\t' + 'Five ' + rankNames[i] + 's' + '\n');
+        stream.write(fiveK + '\t\t' + 6 + '\t\t' + 'Five ' + rankNames[i] + 's' + '\n');
+        totalHands+=6;
     }
 }
 
 function fiveFlush(stream) {
-    function fourFlush(stream) { 
-        for(let i = 0; i < ranks.length - 4; i++) {
-            for(let j = i + 1; j < ranks.length - 3; j++) {
-                for(let k = j + 1; k < ranks.length - 2; k++) {
-                    for(let l = k + 1; l < ranks.length - 1; l++) {
-                        for(let m = l + 1; m < ranks.length; m++) {
-                            const flush = ranks[i] + ranks[j] + ranks[k] + ranks[l] + ranks[m];
-                            stream.write(flush + '\t' + 6 + '\t' + rankNAmes[i] + ' high 5 flush' + '\n');
+    for(let i = 0; i < ranks.length - 4; i++) {
+        for(let j = i + 1; j < ranks.length - 3; j++) {
+            for(let k = j + 1; k < ranks.length - 2; k++) {
+                for(let l = k + 1; l < ranks.length - 1; l++) {
+                    for(let m = l + 1; m < ranks.length; m++) {
+                        const flush = ranks[i] + ranks[j] + ranks[k] + ranks[l] + ranks[m];
+                        //Special case A9876
+                        if (!(straights.includes(flush) || straights.includes(flush.substring(1) + ranks[i]))) {
+                            stream.write(flush + '\t\t' + 6 + '\t\t' + rankNames[i] + ' high 5 flush' + '\n');
+                            totalHands+=6;
                         }
                     }
                 }
@@ -57,7 +80,8 @@ function fourKind(stream) {
         for(let j = 0; j < ranks.length; j++) {
             if(j !== i) {
                 const fourK = ranks[i].repeat(4) + ranks[j];
-                stream.write(fourK + '\t' + 90 + '\t' + 'Four ' + rankNames[i] + 's' + '\n');
+                stream.write(fourK + '\t\t' + 90 + '\t\t' + 'Four ' + rankNames[i] + 's' + '\n');
+                totalHands+=90;
             }
         }
     }
@@ -68,7 +92,8 @@ function fullHouse(stream) {
         for(let j = 0; j < ranks.length; j++) {
             if(i !== j) {
                 const fullH = ranks[i].repeat(3) + ranks[j].repeat(2);
-                stream.write(fullH + '\t' + 300 + '\t' + rankNames[i] + 's full of ' + rankNames[j] + 's' + '\n');
+                stream.write(fullH + '\t\t' + 300 + '\t\t' + rankNames[i] + 's full of ' + rankNames[j] + 's' + '\n');
+                totalHands+=300;
             }
         }
     }
@@ -81,7 +106,8 @@ function fourFlush(stream) {
                 for(let l = k + 1; l < ranks.length; l++) {
                     for(let m = 0; m < ranks.length; m++) {
                         const flush = ranks[i] + ranks[j] + ranks[k] + ranks[l] + ranks[m];
-                        stream.write(flush + '\t' + 270 + '\t' + rankNames[i] + ' high 4 flush' + '\n');
+                        stream.write(flush + '\t\t' + 30 + '\t\t' + rankNames[i] + ' high 4 flush' + '\n');
+                        totalHands+=30;
                     }
                 }
             }
@@ -92,8 +118,8 @@ function fourFlush(stream) {
 function straight(stream) {
     for(i = 0; i < ranks.length - 3; i++) { 
         const str = ranks[i] + ranks[i + 1] + ranks[i + 2] + ranks[i + 3] + ranks[(i + 4)%ranks.length];
-        straights.push(str); 
-        stream.write(str + '\t' + 7740 + '\t' + rankNames[i] + ' high straight' + '\n');
+        stream.write(str + '\t\t' + 7740 + '\t' + rankNames[i] + ' high straight' + '\n');
+        totalHands+=7740;
     }
 }
 
@@ -103,7 +129,8 @@ function threeKind(stream) {
             for(let k = j + 1; k < ranks.length; k++) {
                 if(j !== i && i !== k) {
                     const threeK = ranks[i].repeat(3) + ranks[j] + ranks[k];
-                    stream.write(threeK + '\t' + 720 + '\t' + 'Three ' + rankNames[i] + 's' + '\n');
+                    stream.write(threeK + '\t\t' + 720 + '\t\t' + 'Three ' + rankNames[i] + 's' + '\n');
+                    totalHands+=720;
                 }
             }
         }
@@ -112,10 +139,13 @@ function threeKind(stream) {
 
 function twoPair(stream) {
     for(let i = 0; i < ranks.length - 1; i++) {
-        for(let j = i; j < ranks.length; j++) {
+        for(let j = i + 1; j < ranks.length; j++) {
             for(let k = 0; k < ranks.length; k++) {
                 const twoP = ranks[i].repeat(2) + ranks[j].repeat(2) + ranks[k]; 
-                stream.write(twoP + '\t' + 1350 + '\t' + rankNames[i] + 's and ' + rankNames[j] + 's' + '\n');
+                if(k !== i && k !== j) {
+                    stream.write(twoP + '\t\t' + 1350 + '\t\t' + rankNames[i] + 's and ' + rankNames[j] + 's' + '\n');
+                    totalHands+=1350;
+                }
             }
         }
     }
@@ -126,8 +156,11 @@ function onePair(stream) {
         for(let j = 0; j < ranks.length - 2; j++) {
             for(let k = j + 1; k < ranks.length - 1; k++){
                 for(let l = k + 1; l < ranks.length; l++) {
-                    const oneP = ranks[i].repeat(2) + ranks[j] + ranks[k] + ranks[l];
-                    stream.write(oneP + '\t' + 3210  + '\t' + 'Pair of ' + rankNames[i] + 's' + '\n');
+                    if(j !== i && k !== i && l !== i) {
+                        const oneP = ranks[i].repeat(2) + ranks[j] + ranks[k] + ranks[l];
+                        stream.write(oneP + '\t\t' + 3210  + '\t\t' + 'Pair of ' + rankNames[i] + 's' + '\n');
+                        totalHands+=3210;
+                    }
                 }
             }
         }
@@ -141,8 +174,9 @@ function highCard(stream) {
                 for(let l = k + 1; l < ranks.length - 1; l++) {
                     for(let m = l + 1; m < ranks.length; m++) {
                         const hc = ranks[i] + ranks[j] + ranks[k] + ranks[l] + ranks[m]; 
-                        if(!straights.includes(hc)) {
-                            stream.write(hc + '\t' + 7740 + '\t' + rankNames[i] + '-high' + '\n');
+                        if(!(straights.includes(hc) || straights.includes(hc.substring(1) + ranks[i]))) {
+                            stream.write(hc + '\t\t' + 7740 + '\t\t' + rankNames[i] + '-high' + '\n');
+                            totalHands+=7740;
                         }
                     }
                 }
